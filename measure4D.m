77f6a867803887125f365633bd4fb57be6d9f434
mjt320 <36667519+mjt320@users.noreply.github.com>
2019-07-08 16:58:32 +0100

    Update measure4D.m
    Include standard deviation in output.

--- measure4D.m
+++ measure4D.m
@@ -22,11 +22,13 @@ N=size(signal,4);
 
 ROIData.mean=nan(N,1);
 ROIData.median=nan(N,1);
+ROIData.SD=nan(N,1);
 
 for n=1:N %loop through time points of 4D image
     signalThisTimepoint=squeeze(signal(:,:,:,n));
     ROIData.mean(n,1) = nanmean(signalThisTimepoint(mask==1));
     ROIData.median(n,1) = nanmedian(signalThisTimepoint(mask==1));
+    ROIData.SD(n,1) = nanstd(signalThisTimepoint(mask==1));
 end
 
 end
